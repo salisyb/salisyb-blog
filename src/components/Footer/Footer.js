72f6863e4ec490f0ab0aae0ed51871680024de63
Salisu Abubakar <salisbell404@gmail.com>
2024-04-17 11:27:11 +0100

    initialize blog

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/salisyb'
+      className='link footer__link'
+    >
+      Salisu Abubakar
+    </a>
+  </footer>
+)
+
+export default Footer
